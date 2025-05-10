5f016f7a429f5b0974478ea99938eacd43672d5b
Pl7x <pedrocabral1707@gmail.com>
2025-05-09 21:20:40 -0300

    Update BD_Projeto.sql

--- BD_Projeto.sql
+++ BD_Projeto.sql
@@ -1,43 +1,33 @@
-CREATE TABLE tb_Cliente (
-    id_Cliente serial PRIMARY KEY,
-    no_Cliente VARCHAR(50) NOT NULL,
-    cpf_Cliente VARCHAR(14) UNIQUE NOT NULL,
-    email_Cliente VARCHAR(50) UNIQUE NOT NULL,
-    nasc_Cliente DATE NOT NULL,
-    tel_Cliente VARCHAR(15),
-    end_Cliente VARCHAR(100) NOT NULL,
-    sexo_Cliente VARCHAR(1) NOT NULL,
-    dt_CC TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
 CREATE TABLE tb_Funcionario (
-    id_Fun serial PRIMARY KEY,
+    id_Fun  serial PRIMARY KEY,
     no_Fun VARCHAR(50) NOT NULL,
     cpf_Fun VARCHAR(14) unique NOT NULL,
-    email_Fun VARCHAR(50) NOT NULL,
+    email_Fun VARCHAR(50),
     nasc_Fun DATE NOT NULL,
     tel_Fun VARCHAR(15),
-    end_Fun VARCHAR(100),
-    sexo_Fun VARCHAR(1),
-    senha_Fun VARCHAR(12),
-    salario DECIMAL(10,2),
-    dt_CF TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    end_Fun VARCHAR(100)not null,
+    sexo_Fun VARCHAR(1) not null check(sexo_fun in('F','M')),
+    senha_Fun VARCHAR(12) not null,
+    salario DECIMAL(10,2) not null,
+    dt_CF TIMESTAMP DEFAULT CURRENT_TIMESTAMP not null
 );
 
 CREATE TABLE tb_Combustivel (
-    id_Combustivel serial PRIMARY KEY,
+    id_Combustivel  serial PRIMARY KEY,
     no_Combustivel VARCHAR(50) NOT NULL,
-    estoque_litros DECIMAL(10,2) NOT NULL,
-    estoque_atual DECIMAL(10,2),
+    cap_max DECIMAL(10,2) NOT NULL,
+    estoque_atual DECIMAL(10,2)not null,
     preco_litro DECIMAL(10,2) NOT NULL
 );
 
 CREATE TABLE tb_Compra (
-    id_Compra serial PRIMARY KEY,
-    id_Cliente INTEGER REFERENCES tb_Cliente(id_Cliente),
-    id_Fun INTEGER REFERENCES tb_Funcionario(id_Fun),
-    id_Combustivel INTEGER REFERENCES tb_Combustivel(id_Combustivel),
+    id_Compra SERIAL PRIMARY KEY,
+    id_Fun INTEGER,
+    id_Combustivel INTEGER,
     qntd_Litros DECIMAL(10,2) NOT NULL,
     valor_Pago DECIMAL(10,2) NOT NULL,
-    dt_venda TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    forma_pagamento VARCHAR(10) NOT NULL CHECK (forma_pagamento IN ('dinheiro', 'pix', 'cartao')),
+    dt_venda TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_Fun) REFERENCES tb_Funcionario(id_Fun),
+    FOREIGN KEY (id_Combustivel) REFERENCES tb_Combustivel(id_Combustivel)
 );
