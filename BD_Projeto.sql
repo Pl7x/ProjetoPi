ddaa5e4a9894cfc3eef5c4c5aff9e372b8f26992
Pl7x <pedrocabral1707@gmail.com>
2025-05-22 20:27:12 -0300

    Update BD_Projeto.sql

--- BD_Projeto.sql
+++ BD_Projeto.sql
@@ -1,22 +1,23 @@
 CREATE TABLE tb_Funcionario (
-    id_Fun  serial PRIMARY KEY,
+    id_Fun serial PRIMARY KEY,
     no_Fun VARCHAR(50) NOT NULL,
-    cpf_Fun VARCHAR(14) unique NOT NULL,
+    cpf_Fun VARCHAR(14) UNIQUE NOT NULL,
     email_Fun VARCHAR(50),
     nasc_Fun DATE NOT NULL,
     tel_Fun VARCHAR(15),
-    end_Fun VARCHAR(100)not null,
-    sexo_Fun VARCHAR(1) not null check(sexo_fun in('F','M')),
-    senha_Fun VARCHAR(12) not null,
-    salario DECIMAL(10,2) not null,
-    dt_CF TIMESTAMP DEFAULT CURRENT_TIMESTAMP not null
+    end_Fun VARCHAR(100) NOT NULL,
+    sexo_Fun VARCHAR(1) NOT NULL CHECK (sexo_Fun IN ('F','M')),
+    senha_Fun VARCHAR(12) NOT NULL,
+    salario DECIMAL(10,2) NOT NULL,
+    dt_CF TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    Status BOOLEAN NOT NULL DEFAULT TRUE -- TRUE = Ativo, FALSE = Inativo
 );
 
 CREATE TABLE tb_Combustivel (
-    id_Combustivel  serial PRIMARY KEY,
+    id_Combustivel serial PRIMARY KEY,
     no_Combustivel VARCHAR(50) NOT NULL,
     cap_max DECIMAL(10,2) NOT NULL,
-    estoque_atual DECIMAL(10,2)not null,
+    estoque_atual DECIMAL(10,2) NOT NULL,
     preco_litro DECIMAL(10,2) NOT NULL
 );
 
